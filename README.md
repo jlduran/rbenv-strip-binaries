7a509df96cd8643be80ee1d2bcbe6751939154e0
Jose Luis Duran <jlduran@users.noreply.github.com>
2020-04-10 12:37:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# rbenv-strip-binaries
+
+This rbenv plugin hooks into the `rbenv install` command to
+automatically strip binaries every time you install a new version of
+Ruby.
+
+## Installation
+
+Make sure you have the latest rbenv and ruby-build versions, then run:
+
+    git clone https://github.com/jlduran/rbenv-strip-binaries.git $(rbenv root)/plugins/rbenv-strip-binaries
+
+## Usage
+
+rbenv-strip-binaries automatically strips binaries every time you
+successfully install a new version of Ruby with `rbenv install`.
---
+++ etc/rbenv.d/install/strip-binaries.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+after_install strip_binaries
+
+strip_binaries()
+{
+	# Strip binaries only after successfully installing Ruby.
+	[ "$STATUS" = "0" ] || return 0
+
+	STRIP_CMD="strip -s"
+	PREFIX_PATH="${RBENV_ROOT}/versions/${VERSION_NAME}"
+	MAJOR_VERSION=${VERSION_NAME%.*}
+	SHLIB_VER=${MAJOR_VERSION/./}
+
+	${STRIP_CMD} "${PREFIX_PATH}/bin/ruby"
+	${STRIP_CMD} "${PREFIX_PATH}/lib/libruby.so.${SHLIB_VER}"
+	find "${PREFIX_PATH}/lib/ruby/${MAJOR_VERSION}.0" -type f -name "*.so" -exec ${STRIP_CMD} {} \;
+}

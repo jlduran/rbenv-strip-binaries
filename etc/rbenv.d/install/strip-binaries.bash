5aee5ba4d76489dcdeaa8b28e8980b5b95df6258
Jose Luis Duran <jlduran@users.noreply.github.com>
2020-04-10 17:09:07 -0300

    Account for different OSs (#1)

--- etc/rbenv.d/install/strip-binaries.bash
+++ etc/rbenv.d/install/strip-binaries.bash
@@ -7,12 +7,28 @@ strip_binaries()
 	# Strip binaries only after successfully installing Ruby.
 	[ "$STATUS" = "0" ] || return 0
 
-	STRIP_CMD="/usr/bin/strip"
 	PREFIX_PATH="${RBENV_ROOT}/versions/${VERSION_NAME}"
 	MAJOR_VERSION=${VERSION_NAME%.*}
-	SHLIB_VER=${MAJOR_VERSION/./}
+
+	case $(uname -s) in
+	Darwin)
+		STRIP_CMD="/usr/bin/strip -x"
+		LIBRUBY_FILE="libruby.${MAJOR_VERSION}.dylib"
+		SO_FILES="*.bundle"
+		;;
+	FreeBSD)
+		STRIP_CMD="/usr/bin/strip"
+		LIBRUBY_FILE="libruby.so.${MAJOR_VERSION/./}"
+		SO_FILES="*.so"
+		;;
+	*)
+		STRIP_CMD="/usr/bin/strip"
+		LIBRUBY_FILE="libruby.so.${VERSION_NAME}"
+		SO_FILES="*.so"
+		;;
+	esac
 
 	${STRIP_CMD} "${PREFIX_PATH}/bin/ruby"
-	${STRIP_CMD} "${PREFIX_PATH}/lib/libruby.so.${SHLIB_VER}"
-	find "${PREFIX_PATH}/lib/ruby/${MAJOR_VERSION}.0" -type f -name "*.so" -exec ${STRIP_CMD} {} \;
+	${STRIP_CMD} "${PREFIX_PATH}/lib/${LIBRUBY_FILE}"
+	find "${PREFIX_PATH}/lib/ruby/${MAJOR_VERSION}.0" -type f -name "${SO_FILES}" -exec ${STRIP_CMD} {} \;
 }

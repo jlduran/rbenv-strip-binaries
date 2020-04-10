43df9b87b797d51eff5f4363687e5418d72c8dfc
Jose Luis Duran <jlduran@users.noreply.github.com>
2020-04-10 15:42:14 -0300

    Add -x flag for macOS

--- etc/rbenv.d/install/strip-binaries.bash
+++ etc/rbenv.d/install/strip-binaries.bash
@@ -7,16 +7,17 @@ strip_binaries()
 	# Strip binaries only after successfully installing Ruby.
 	[ "$STATUS" = "0" ] || return 0
 
-	STRIP_CMD="/usr/bin/strip"
 	PREFIX_PATH="${RBENV_ROOT}/versions/${VERSION_NAME}"
 	MAJOR_VERSION=${VERSION_NAME%.*}
 
 	case $(uname -s) in
 	Darwin)
+		STRIP_CMD="/usr/bin/strip -x"
 		LIBRUBY_FILE="libruby.${MAJOR_VERSION}.dylib"
 		SO_FILES="*.bundle"
 		;;
 	*)
+		STRIP_CMD="/usr/bin/strip"
 		LIBRUBY_FILE="libruby.so.${MAJOR_VERSION/./}"
 		SO_FILES="*.so"
 		;;
